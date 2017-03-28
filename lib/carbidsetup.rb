9e528279c210cc92cf37650c3fdd659a67a07764
Do Dinh Thy  Son <dodinhthyson@gmail.com>
2017-03-28 17:07:47 +0700

    Renamed exec to carbidsetup

--- lib/carbidsetup.rb
+++ lib/carbidsetup.rb
@@ -1,19 +1,30 @@
 require "carbidsetup/version"
 require 'highline'
+require 'cocoapods'
 module Carbidsetup
   class Main < HighLine
     def run 
-      project_name = ""
-      if project_name 
+      project_path = Dir.glob('./*.xcodeproj').first
+      if project_file
         say("Setting up files for project #{project_name}")
       else
         say("Couldn't find *.xcproject file. Please cd into the correct folder of the project file")
         abort
       end 
+      podfile_path = Dir.glob('./podfile').first
+      unless podfile_path
+        say("Couldn't find podfile. Please pod init first before running this")
+      end 
       
-      info_plist = File.open('./#{project_name}/info.plist')
-      info_plist_xml = Nokogiri.xml(info_plist)
+      project_name = project_path.split('.').first
       
+      info_plist_path = File.open('./#{project_name}/info.plist')
+      info_plist = Nokogiri.xml(info_plist)
+      podfile = File.open(podfile_path)
+      
+      puts '#{podfile}'
+      puts '#{info_plist}'
+
       choose do |menu|
         menu.prompt = "Tab or drawer menu ?"
         menu.choice(:tab) { cli.say(" Do nothing ") }
