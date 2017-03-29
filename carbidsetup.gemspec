352d91f704ca8009abde11e577a5d8972deb289c
Do Dinh Thy  Son <dodinhthyson@gmail.com>
2017-03-29 17:41:57 +0700

    “Just work” version finished. Code needs refactoring.

--- carbidsetup.gemspec
+++ carbidsetup.gemspec
@@ -33,6 +33,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.14"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "pry"
-  spec.add_runtime_dependency "nokogiri", "~> 1.7.1"
   spec.add_runtime_dependency "highline"
 end
--- lib/carbidsetup.rb
+++ lib/carbidsetup.rb
@@ -1,10 +1,11 @@
 require "carbidsetup/version"
 require 'highline'
-require 'nokogiri'
 module Carbidsetup
   class Main < HighLine
-    @pods = Array.new
+    attr_accessor :pods
+    attr_accessor :info_plist_path
     def run 
+      @pods = []
       project_path = Dir.glob('*.xcodeproj').first
       if project_path
         say("Setting up files for project #{project_path}")
@@ -19,16 +20,11 @@ module Carbidsetup
       
       project_name = project_path.split('.').first
       
-      info_plist_path = File.open("./#{project_name}/info.plist")
-      @info_plist = Nokogiri.XML(info_plist_path)
-      podfile = File.open(podfile_path)
-      
-      puts "#{podfile}"
-      puts "#{@info_plist}"
+      @info_plist_path = "./#{project_name}/info.plist"
       
       choose do |menu|
         menu.prompt = "Add drawer menu ?"
-        menu.choice(:yes) { pods.append "SlideMenuControllerSwift" }
+        menu.choice(:yes) { @pods << "SlideMenuControllerSwift" }
         menu.choice(:no) { puts(" Do nothing and continue")}
         menu.default = :no
       end 
@@ -46,74 +42,73 @@ module Carbidsetup
         menu.choice(:no) { say "To the next question"}
       end 
       
-      add_to_podfile pods_to_string
-      `pod install`
+      add_line_under_line("podfile",/use_frameworks!/, pods_to_string)
+      # `pod install` Trying to run pod install 
     end 
     
     def setup_google
       url = 'https://developers.google.com/mobile/add?platform=ios&cntapi=signin&cntapp=Default%20Demo%20App&cntpkg=com.google.samples.quickstart.SignInExample'
       `open #{url}`
-      "Please setup the project name and download the config file")
-      reversed_id = ask("What is the REVERSED_ID ? It can be found in the google-info.plist file you just downloaded") { |id| id.strip! }
-      if reversed_id
-        add_url_scheme reversed_id
-        
-        puts "Copy the delegate service file into the project"
-      end 
-      pods.append "Google/SignIn"      
+      puts "Please setup the project name and download the config file"
+      reversed_id = ask("What is the REVERSED_ID ? It can be found in the google-info.plist file you just downloaded")
+      add_url_scheme(reversed_id)
+      puts "Copy the delegate service file into the project"
+      
+      pods << "Google/SignIn"      
     end 
     
     def setup_facebook
       url = 'https://developers.facebook.com/docs/facebook-login/ios'
       `open #{url}`
-      bundle_id = say("Please add the current project bundle id to the facebook developer settings")
+      puts "Please add the current project bundle id to the facebook developer settings"
       appID = ask("Please return your appID") { |q| q.validate = /\Afb\d+\z/ }
       display_name = ask("Please return your display name ?")
       url_scheme = ask("Please return your url scheme ?")
       if appID 
         add_facebook_permissions
-
-        dict_root_node.add_child 
+        
+        add_line_under_line(@info_plist_path, /<dict>/,
         "<key>FacebookAppID</key>
         <string>#{appID}</string>
         <key>FacebookDisplayName</key>
-        <string>#{display_name}</string>"
-
+        <string>#{display_name}</string>
+        ")
+        
         add_url_scheme url_scheme
-
+        
         puts "Adding the delegate setup service file to the project"
       end 
-      pods.append "FBSDKCoreKit"
-      pods.append "FBSDKLoginKit"
-    end 
-    
-    def url_scheme_node
-      @info_plist.at('key:contains("CFBundleURLTypes")').next_element
-    end 
-    
-    def dict_root_node 
-    end 
+      pods << "FBSDKCoreKit"
+      pods << "FBSDKLoginKit"
+    end  
     
     def add_url_scheme(url)
-      url_scheme_node.add_child 
+      # if !line_exist_in_file?(@info_plist_path, /\A<key>CFBundleURLTypes<\/key>\z/)
+      #   add_line_under_line(@info_plist_path,/<dict>/, "<key>CFBundleURLTypes</key>
+      #   ") 
+      # end
+      
+      add_line_under_line(@info_plist_path, /\A<key>CFBundleURLTypes<\/key>\z/,
       "<dict>
-			<key>CFBundleTypeRole</key>
-			<string>Editor</string>
-			<key>CFBundleURLSchemes</key>
-			<array>
+      <key>CFBundleTypeRole</key>
+      <string>Editor</string>
+      <key>CFBundleURLSchemes</key>
+      <array>
       <string>#{url}</string>
-			</array>
-      </dict>"
+      </array>
+      </dict>
+      ")
     end
     
-    def add_facebook_permissions    
-      @info_plist.at('key:contains("CFBundleURLTypes")').add_next_sibling "<key>LSApplicationQueriesSchemes</key>
+    def add_facebook_permissions  
+      add_line_under_line(@info_plist_path, /<dict>/, "<key>LSApplicationQueriesSchemes</key>
       <array>
       <string>fbapi</string>
       <string>fb-messenger-api</string>
       <string>fbauth2</string>
       <string>fbshareextension</string>
-      </array>"
+      </array>
+      ")
     end 
     
     def new_xml_node(name, content = "")
@@ -122,14 +117,28 @@ module Carbidsetup
     end 
     
     def pods_to_string
-      @pods.map { |pod| "/n pod #{pod} " }.join("")
+      @pods.map { |pod| "pod #{pod} " }.join("")
+    end
+    
+    def add_line_under_line(file, catch_line, line_to_add)
+      f = File.open(file, "r+")
+      output = ""
+      f.each do |line| 
+        if catch_line.match(line.strip)
+          output << line
+          output << line_to_add
+        else
+          output << line  
+        end
+      end
+      f.close
+      f =  File.open(file, "w") { |f| f << output }
+      f.close
+    end
+    
+    def line_exist_in_file?(file, line)
+      File.readlines(file).grep(line).size > 0    
     end 
     
-    def add_to_podfile(line_to_add)
-      use_framework_line = 0 
-      File.open("podfile").each do |line|
-        puts line_to_add if /use_frameworks!/.match(line)
-      end      
-    end 
   end 
 end
