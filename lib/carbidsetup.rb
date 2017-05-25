c9e15a25965ea0fc8761c9202f2203b36c07fae5
Do Dinh Thy  Son <dodinhthyson@gmail.com>
2017-05-25 11:17:33 +0700

    Finish with #2 and #3 : - Remove all the old code including Login and List view - Finish a download system which will add all swift file to the boilerplate project - Convenient for Git and Xcodeproj

--- lib/carbidsetup.rb
+++ lib/carbidsetup.rb
@@ -1,5 +1,8 @@
 require "carbidsetup/version"
 require 'highline'
+require 'pathname'
+require 'find'
+
 require "carbidsetup/command/loginscreen"
 require "carbidsetup/command/listscreen"
 require "carbidsetup/command/facebooksetup"
@@ -7,21 +10,20 @@ require "carbidsetup/command/googlesetup"
 require "carbidsetup/command/iosproject"
 require "carbidsetup/user_interface"
 require "carbidsetup/xcodeproj"
-require 'pathname'
 
 module Carbidsetup
   class CarbidsetupError < StandardError; end 
   class Main < HighLine
     attr_accessor :project_name
     
+    BOILERPLATE_GIT_URL = 'https://github.com/Innovatube/ios-clean-boilerplate.git'
+    BOILERPLATE_MAIN_GROUP = 'CleanBoilerplate'
+
     def run 
       swagger
       setup_xcode_project
-      download_boilerplate
-      main_project = Xcodeproj::Project.main_project('ios-clean-boilerplate')
-      download_and_add 'https://github.com/SimplicityMobile/Simplicity.git', main_project
-      # login_view_options
-      # list_view_options
+      main_project = download_boilerplate
+      download_and_add main_project, 'https://github.com/SimplicityMobile/Simplicity.git' 
       UserInterface.prints_warnings
     end
     
@@ -49,42 +51,6 @@ module Carbidsetup
       `swagger-codegen generate -i #{filename} -l swift3 -o ./#{project_name}/API`
     end 
     
-    def login_view_options
-      options = Hash.new
-      choose do |menu| 
-        menu.prompt = "   Add login view ?"
-        menu.choice(:yes)
-        menu.choice(:no) { return }
-      end 
-      choose do |menu|
-        menu.prompt = "   Additional authentication options ?"
-        menu.choice(:facebook_google) { options["authentication"] = "facebook_google" }
-        menu.choice(:facebook) { options["authentication"] = "facebook" }
-        menu.choice(:google) { options["authentication"] = "google" }
-        menu.choice(:no) { options["authentication"] = "" }
-        menu.default = :no
-      end 
-      # Loginscreen.new(@project_name, options).run
-    end 
-    
-    def list_view_options
-      options = Hash.new
-      choose do |menu|
-        menu.prompt = "   Add list view ?"
-        menu.choice(:yes) 
-        menu.choice(:no) { return }
-        menu.default = :yes 
-      end 
-      
-      choose do |menu|
-        menu.prompt = "   Add drawer menu ?"
-        menu.choice(:yes) { options["drawer"] = true }
-        menu.choice(:no)
-        menu.default = :no
-      end
-      # Listscreen.new(@project_name, options).run
-    end 
-    
     def ask_check_project
       count = 0 
       begin 
@@ -102,20 +68,19 @@ module Carbidsetup
     
     def download_boilerplate
       UserInterface.notice 'Make sure you have read access to Innovatube\'s Github repo'
-      Git.download_git_master 'https://github.com/Innovatube/ios-clean-boilerplate.git'
+      Git.download_git_master BOILERPLATE_GIT_URL
+      Xcodeproj::Project.main_project(Git.repo_name BOILERPLATE_GIT_URL)
     end 
     
-    def download_and_add(url, main_project)
+    def download_and_add(main_project, url)
+      project = main_project[BOILERPLATE_MAIN_GROUP]
       folder_name = Git.repo_name(url)
-      Dir.chdir(main_project.path.dirname)
-      Git.download_git_master(url)
-      # 'https://github.com/SimplicityMobile/Simplicity.git'
-      path = File.join(folder_name,folder_name,'*.swift')
-      Dir.glob(File.join(folder_name,folder_name,'*.swift')) do |path|
-        main_project.new_file "./#{path}"
+      Git.download_git_master(url, File.join(main_project.path.dirname,BOILERPLATE_MAIN_GROUP, Git.repo_name(url)))
+      Find.find(File.join(main_project.path.dirname,BOILERPLATE_MAIN_GROUP, folder_name)) do |path|
+        # Use the .new_file method allow both project / group in project to add file
+        project.new_file(path) if path =~ /.*\.swift$/
       end
       main_project.save
-      Dir.chdir('..')
     end
     
   end 
--- lib/carbidsetup/git.rb
+++ lib/carbidsetup/git.rb
@@ -9,14 +9,14 @@ module Carbidsetup
                 /[\w-]+.git/.match(git_url).to_s.chomp '.git'
             end 
             
-            def download_git_master(url)
-                git_shallow_clone(url, "master")
+            def download_git_master(url, dest = self.repo_name(url))
+                git_shallow_clone(url, "master", dest)
                 remove_git
             end
             
-            def git_shallow_clone(url, branch)
+            def git_shallow_clone(url, branch, dest = self.repo_name(url) )
                 if git_url_isValid?(url)
-                    `git clone --depth 1 --branch #{branch} #{url} #{self.repo_name(url)}`
+                    `git clone --depth 1 --branch #{branch} #{url} #{dest}`
                 elsif 
                     raise CarbidsetupError, 'Invalid git repo url'
                 end
